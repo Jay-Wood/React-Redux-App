d9aefc39797c334dd031b619f5120fb7592138ca
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-08-15 13:07:40 -0700

    Basic setup complete

---
+++ songsterr-tabs/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Find NBA Stats!</h1> 
+      </header>
+      <div >
+         <p>Player/stats List here</p> 
+         <p> Maybe filters/search functionality</p> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
